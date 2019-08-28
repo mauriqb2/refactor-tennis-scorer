5e643c5387d31afa7abe86b4da1061785bf4c3cc
mauricio <mauriqb2@gmail.com>
2019-08-28 00:42:04 -0400

    Extract boolean and String methods

--- src/TennisGame2.java
+++ src/TennisGame2.java
@@ -38,26 +38,14 @@ public class TennisGame2 implements TennisGame
 	private String literal(String score) {
 		if (P1point>P2point && P1point < 4)
         {
-            if (P1point==2)
-                P1res="Thirty";
-            if (P1point==3)
-                P1res="Forty";
-            if (P2point==1)
-                P2res="Fifteen";
-            if (P2point==2)
-                P2res="Thirty";
+			P1res = getLiteral(P1point);
+            P2res = getLiteral(P2point);
             score = P1res + "-" + P2res;
         }
         if (P2point>P1point && P2point < 4)
         {
-            if (P2point==2)
-                P2res="Thirty";
-            if (P2point==3)
-                P2res="Forty";
-            if (P1point==1)
-                P1res="Fifteen";
-            if (P1point==2)
-                P1res="Thirty";
+        	P2res = getLiteral(P2point);
+        	P1res = getLiteral(P1point);
             score = P1res + "-" + P2res;
         }
 		return score;
@@ -66,12 +54,7 @@ public class TennisGame2 implements TennisGame
 	private String all(String score) {
 		if (P1point == P2point && P1point < 4)
         {
-            if (P1point==0)
-                score = "Love";
-            if (P1point==1)
-                score = "Fifteen";
-            if (P1point==2)
-                score = "Thirty";
+            score = getLiteral(P1point);
             score += "-All";
         }
 		return score;
@@ -80,54 +63,63 @@ public class TennisGame2 implements TennisGame
 	private String love(String score) {
 		if (P1point > 0 && P2point==0)
         {
-            if (P1point==1)
-                P1res = "Fifteen";
-            if (P1point==2)
-                P1res = "Thirty";
-            if (P1point==3)
-                P1res = "Forty";
-            
-            P2res = "Love";
+            P1res = getLiteral(P1point);
+            P2res = getLiteral(P2point);
             score = P1res + "-" + P2res;
         }
         if (P2point > 0 && P1point==0)
         {
-            if (P2point==1)
-                P2res = "Fifteen";
-            if (P2point==2)
-                P2res = "Thirty";
-            if (P2point==3)
-                P2res = "Forty";
-            
-            P1res = "Love";
+        	P2res = getLiteral(P2point);
+            P1res = getLiteral(P1point);
             score = P1res + "-" + P2res;
         }
 		return score;
 	}
 
+	private String getLiteral(int playerPoint) {
+		String literal="";
+		if (P1point==0)
+			literal = "Love";
+		if (playerPoint==1)
+		    literal = "Fifteen";
+		if (playerPoint==2)
+		    literal = "Thirty";
+		if (playerPoint==3)
+		    literal = "Forty";
+		return literal;
+	}
+
 	private String advantage(String score) {
-		if (P1point > P2point && P2point >= 3)
+		if (hasFirstAdvantage(P1point, P2point))
         {
             score = "Advantage player1";
         }
         
-        if (P2point > P1point && P1point >= 3)
+        if (hasFirstAdvantage(P2point, P1point))
         {
             score = "Advantage player2";
         }
 		return score;
 	}
 
+	private boolean hasFirstAdvantage(int playerOne, int playerTwo) {
+		return playerOne > playerTwo && playerTwo >= 3;
+	}
+
 	private String win(String score) {
-		if (P1point>=4 && P2point>=0 && (P1point-P2point)>=2)
+		if (isfirstWinner(P1point, P2point))
         {
             score = "Win for player1";
         }
-        if (P2point>=4 && P1point>=0 && (P2point-P1point)>=2)
+        if (isfirstWinner(P2point, P1point))
         {
             score = "Win for player2";
         }
 		return score;
+	}
+
+	private boolean isfirstWinner(int playerOne, int playerTwo) {
+		return playerOne>=4 && playerTwo>=0 && (playerOne-playerTwo)>=2;
 	}
     
     public void SetP1Score(int number){
